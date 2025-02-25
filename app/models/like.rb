fbb9fc444d93322c4603d475ab0b556b8780a26a
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-02-25 18:33:45 +0000

    direct associations photos

--- app/models/like.rb
+++ app/models/like.rb
@@ -21,6 +21,6 @@ class Like < ApplicationRecord
   ## Direct associations
 
   # Like#fan: returns a row from the users table associated to this like by the fan_id column
-
+  
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
 end
